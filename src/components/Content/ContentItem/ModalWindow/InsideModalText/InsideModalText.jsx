6f21b29b5f1fce05a16c9ed45cd70fd0181f570f
Chaker <chaker72@mail.ru>
2023-04-10 13:48:50 +0700

    useEffect

--- src/components/Content/ContentItem/ModalWindow/InsideModalText/InsideModalText.jsx
+++ src/components/Content/ContentItem/ModalWindow/InsideModalText/InsideModalText.jsx
@@ -6,26 +6,26 @@ const InsideModalText = (props) => {
     return (
         <div className={styles.text}>
             <h1>{data.name}</h1>
-            <ul>
-                <li>Рост: 133 см</li>
-                <li>Вес: 200 кг</li>
-                <li>Цвет волос: белый</li>
-                <li>Цвет кожи: светлый</li>
-                <li>Цвет глаз: карий</li>
-                <li>Год рождения: дадатогда</li>
-            </ul>
             {/*<ul>*/}
-            {/*    <li>Вес: {data.mass}</li>*/}
-            {/*    <li>Цвет волос: {data.hair_color}</li>*/}
-            {/*    <li>Цвет кожи: {data.skin_color}</li>*/}
-            {/*    <li>Цвет глаз: {data.eye_color}</li>*/}
-            {/*    <li>Год рождения: {data.birth_year}</li>*/}
-            {/*    <li>Пол: {data.gender}</li>*/}
-            {/*    <li>Место рождения: {data.homeworld}</li>*/}
-            {/*    <li>Фильмы: {data.films}</li>*/}
-            {/*    <li>Транспорты: {data.vehicles}</li>*/}
-            {/*    <li>Звездолеты: {data.starships}</li>*/}
+            {/*    <li>Рост: 133 см</li>*/}
+            {/*    <li>Вес: 200 кг</li>*/}
+            {/*    <li>Цвет волос: белый</li>*/}
+            {/*    <li>Цвет кожи: светлый</li>*/}
+            {/*    <li>Цвет глаз: карий</li>*/}
+            {/*    <li>Год рождения: дадатогда</li>*/}
             {/*</ul>*/}
+            <ul>
+                <li>Вес: {data.mass}</li>
+                <li>Цвет волос: {data.hair_color}</li>
+                <li>Цвет кожи: {data.skin_color}</li>
+                <li>Цвет глаз: {data.eye_color}</li>
+                <li>Год рождения: {data.birth_year}</li>
+                <li>Пол: {data.gender}</li>
+                {/*<li>Место рождения: {data.homeworld}</li>*/}
+                {/*<li>Фильмы: {data.films}</li>*/}
+                {/*<li>Транспорты: {data.vehicles}</li>*/}
+                {/*<li>Звездолеты: {data.starships}</li>*/}
+            </ul>
         </div>
     )
 }
--- src/components/Content/PeoplesPage/PeoplesPage.jsx
+++ src/components/Content/PeoplesPage/PeoplesPage.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './PeoplesPage.module.css';
 import ContentItem from "../ContentItem/ContentItem";
+import axios from "axios";
+import peoplesImg from "../../../assets/images/LEGO_Yoda.jpg"; //default people img
 
 /*Отображаемый материал страницы*/
 const PeoplesPage = (props) => {
+    let baseUrl = 'https://swapi.dev/api';
+    // let setPeoples = () => { -------------Было---------
+    //     if (props.peoples.length === 6) {
+    //         axios.get(baseUrl + '/people/').then(response => {
+    //             props.setPeoples(response.data.results)})}
+    // }
+
+    // const [peoples, setPeoples] = useState(props.peoples); // -------Стало с хуком useState------
+    // function getPeoplesBtn() {
+    //     setPeoples(peoples.length === 6
+    //         ? axios.get(baseUrl + '/people/').then(response => {props.setPeoples(response.data.results)})
+    //         : peoples)
+    // }
+    useEffect(() => { // -------Стало с хуком useEffect------
+            axios.get(baseUrl + '/people/').then(response => {
+                props.setPeoples(response.data.results)
+            })
+        }, []
+    );
     /*Мапинг исходного массива объектов*/
     let contentPeople = props.peoples.map(p =>
-        <ContentItem key={p.id} name={p.name} image={p.img} modalInfo={p}/>)
+        <ContentItem key={p.id} name={p.name} image={p.img != null ? p.img : peoplesImg} modalInfo={p}/>)
     return (
         <div className={styles.content}>
+            {/*<button onClick={getPeoplesBtn}>getPeoples</button>*/}
             {contentPeople}
             <button className={styles.btn} onClick={props.addPeople}>Добавить Эвока</button>
         </div>
